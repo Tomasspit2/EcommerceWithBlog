f137caee2ea6aef0503a0718e25fe7f9d0d5ac86
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-06-01 14:31:53 +0200

    Created the slug for the category and the article routes

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -40,7 +40,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
+    #[Route('/{slug}', name: 'app_category_show', methods: ['GET'])]
     public function show(Category $category, CategoryRepository $categoryRepository): Response
     {
         $categories = $categoryRepository->findAll();
@@ -50,7 +50,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
+    #[Route('/{slug}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
     {
         $form = $this->createForm(CategoryType::class, $category);
@@ -68,7 +68,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
+    #[Route('/{slug}', name: 'app_category_delete', methods: ['POST'])]
     public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
