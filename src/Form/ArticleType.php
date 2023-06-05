83ec9f5257943c6b51bc571c66d7acf3217e1415
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-06-05 21:27:58 +0200

    fixed bug in register

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('content')
+            ->add('image_url')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('description')
+            ->add('author')
+            ->add('categories')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
