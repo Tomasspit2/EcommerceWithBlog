5ce656cbad5744e03bfa4568fba0f79814a975c6
TOMAS SPIT <Tomas.p.spit1@gmail.com>
2023-06-22 14:26:29 +0200

    Added easyAdmin to the project and created the back-office

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Articles')
+            ->setEntityLabelInSingular('Article')
+            ->setPaginatorPageSize(15);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+            TextField::new('slug'),
+            ArrayField::new('category'),
+            ];
+    }
+
+}
---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Categories')
+            ->setEntityLabelInSingular('Category')
+            ->setPaginatorPageSize(10);
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextEditorField::new('description'),
+            TextField::new('slug'),
+        ];
+    }
+
+}
